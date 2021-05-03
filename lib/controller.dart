8d23e7266409de47cd0054efb64e97b206031754
Iury Vasconcelos <iury0393@gmail.com>
2021-05-03 14:18:46 -0300

    Fazendo formulario reativo

--- lib/controller.dart
+++ lib/controller.dart
@@ -1,24 +1,37 @@
 import 'package:mobx/mobx.dart';
+import 'package:mobx_training/models/client.dart';
 part 'controller.g.dart';
 
-class Controller = ControllerBase with _$Controller;
+class Controller = _ControllerBase with _$Controller;
 
-abstract class ControllerBase with Store {
-  @observable
-  String nome = '';
-  @observable
-  String sobrenome = '';
+abstract class _ControllerBase with Store {
+  var client = Client();
 
   @computed
-  String get nomeCompleto => '$nome $sobrenome';
-
-  @action
-  mudarNome(String newNome) {
-    nome = newNome;
+  bool get isValid {
+    return validateName() == null &&
+        validateEmail() == null &&
+        validateCpf() == null;
   }
 
-  @action
-  mudarSobrenome(String newSobrenome) {
-    sobrenome = newSobrenome;
+  String validateName() {
+    if (client.name == null || client.name.isEmpty) {
+      return 'Este campo é obrigatório';
+    }
+    return null;
+  }
+
+  String validateEmail() {
+    if (client.email == null || client.email.isEmpty) {
+      return 'Este campo é obrigatório';
+    }
+    return null;
+  }
+
+  String validateCpf() {
+    if (client.cpf == null || client.cpf.isEmpty) {
+      return 'Este campo é obrigatório';
+    }
+    return null;
   }
 }
--- lib/home.dart
+++ lib/home.dart
@@ -2,37 +2,72 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:mobx_training/controller.dart';
 
-class HomeWidget extends StatelessWidget {
+class HomeWidget extends StatefulWidget {
+  @override
+  _HomeWidgetState createState() => _HomeWidgetState();
+}
+
+class _HomeWidgetState extends State<HomeWidget> {
   final controller = Controller();
+  _textField({String labelText, onChanged, String Function() errorText}) {
+    return TextField(
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(),
+        labelText: labelText,
+        errorText: errorText == null ? null : errorText(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('MobX'),
+        title: Text('Formulário'),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          TextField(
-            decoration: InputDecoration(labelText: 'Nome'),
-            onChanged: controller.mudarNome,
-          ),
-          SizedBox(height: 20),
-          TextField(
-            decoration: InputDecoration(labelText: 'Sobrenome'),
-            onChanged: controller.mudarSobrenome,
-          ),
-          SizedBox(height: 20),
-          Observer(builder: (_) {
-            return Text('${controller.nomeCompleto}');
-          }),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => {},
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            Observer(
+              builder: (_) {
+                return _textField(
+                  labelText: 'Nome',
+                  onChanged: controller.client.changeName,
+                  errorText: controller.validateName,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            Observer(
+              builder: (_) {
+                return _textField(
+                  labelText: 'Email',
+                  onChanged: controller.client.changeEmail,
+                  errorText: controller.validateEmail,
+                );
+              },
+            ),
+            SizedBox(height: 20),
+            Observer(
+              builder: (_) {
+                return _textField(
+                  labelText: 'CPF',
+                  onChanged: controller.client.changeCpf,
+                  errorText: controller.validateCpf,
+                );
+              },
+            ),
+            SizedBox(height: 50),
+            Observer(builder: (_) {
+              return ElevatedButton(
+                onPressed: controller.isValid ? () {} : null,
+                child: Text('Salvar'),
+              );
+            }),
+          ],
+        ),
       ),
     );
   }
